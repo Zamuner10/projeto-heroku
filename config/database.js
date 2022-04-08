2f0e0d53c93d915273a3d857ce23ca343d1fc5ae
Vinicius Klocke <vkzamuner@hotmail.com>
2022-04-08 16:03:10 -0300

    config for heroku

---
+++ config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'cluster0.q6muy.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'strapi_california'),
+        username: env('DATABASE_USERNAME', 'project_california'),
+        password: env('DATABASE_PASSWORD', 'QH9kl3BFR6TGYtb8'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '132bacd3-c15e-4536-aa02-90166bca5f1f'
+};
